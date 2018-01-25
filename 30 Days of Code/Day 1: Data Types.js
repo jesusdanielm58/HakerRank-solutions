5e590f72f816d3cf8b54e868a1a5bd7219f536ea
jesusdanielm58 <33935377+jesusdanielm58@users.noreply.github.com>
2018-01-25 10:26:36 -0400

    Create Day 1: Data Types.js

---
+++ 30 Days of Code/Day 1: Data Types.js
@@ -0,0 +1,14 @@
+
+    // Declare second integer, double, and String variables.
+    var entero=Number(readLine());
+    var decimal=Number(readLine());
+    var cad=readLine();
+    // Read and save an integer, double, and String to your variables.
+    
+    // Print the sum of both integer variables on a new line.
+    console.log(i+entero);
+    // Print the sum of the double variables on a new line.
+    console.log((d+decimal).toFixed(1));
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s+cad);
